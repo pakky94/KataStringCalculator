1dfc83df8bea921d1d5ac3e6d837738f89d6d658
Marco Pacchialat <m.pacchialat@gmail.com>
2022-02-11 09:24:36 +0100

    req 1

--- KataStringCalculator.Tests/CalculatorTests.cs
+++ KataStringCalculator.Tests/CalculatorTests.cs
@@ -4,15 +4,17 @@ namespace KataStringCalculator.Tests
 {
     public class Tests
     {
+        private ICalculator _sut;
+
         [SetUp]
         public void Setup()
         {
+            _sut = new Calculator();
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        [TestCase("", ExpectedResult = 0)]
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("1,2", ExpectedResult = 3)]
+        public int Add_Tests(string input) => _sut.Add(input);
     }
 }
---
+++ KataStringCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataStringCalculator
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(string input)
+        {
+            if (input.Equals(string.Empty))
+                return 0;
+
+            var splitting = input.Split(",");
+            return splitting.Sum(x => int.Parse(x));
+        }
+    }
+}
